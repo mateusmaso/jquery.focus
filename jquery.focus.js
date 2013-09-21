710957570fe2e10738abbe48ed72e7a18c94d5d6
Mateus Maso <m.maso25@gmail.com>
2013-09-21 15:15:45 -0700

    Created jquery.focus.js

---
+++ jquery.focus.js
@@ -0,0 +1,43 @@
+(function($) {
+
+  $.fn.focusEnd = function(){
+    element = this[0];
+
+    if (this.is('textarea')) { 
+        element.focus();
+        element.setSelectionRange(this.val().length, this.val().length); 
+      } else if (this.is('input')) { 
+        var range = element.createTextRange();  
+        range.moveStart('character', this.val().length); 
+        range.select();
+      } else {
+        var range = document.createRange();   
+        range.selectNodeContents(element);    
+        range.collapse(false);          
+        var selection = window.getSelection();  
+        selection.removeAllRanges();        
+        selection.addRange(range);        
+      }
+  };
+
+  $.fn.focusStart = function(){
+    element = this[0];
+
+    if (this.is('textarea')) { 
+        element.focus(); 
+        element.setSelectionRange(0, 0); 
+      } else if (this.is('input')) { 
+        var range = element.createTextRange();  
+        range.moveStart('character', 0); 
+        range.select();
+      } else {
+        var range = document.createRange();   
+        range.selectNodeContents(element);    
+        range.collapse(true);         
+        var selection = window.getSelection();  
+        selection.removeAllRanges();        
+        selection.addRange(range);
+      }
+  };
+
+})(jQuery);
